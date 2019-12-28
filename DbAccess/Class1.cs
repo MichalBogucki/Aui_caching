e87770bbc6ea519350202c06c74d0100a08532c1
Michał Bogucki <bogucek4@gazeta.pl>
2019-12-28 13:22:42 +0100

    working again

--- DbAccess/Class1.cs
+++ DbAccess/Class1.cs
@@ -5,7 +5,8 @@ namespace DbAccess
 {
     public class Class1
     {
-        private readonly string conn = "metadata=res://*/sampleDbEntities.csdl|res://*/sampleDbEntities.ssdl|res://*/sampleDbEntities.msl;provider=System.Data.SqlClient;provider connection string=';data source=tcp:aui2019.database.windows.net,1433;Initial Catalog=sampleDb;Persist Security Info=False;User ID=sql-admin;Password=FitAdm1234;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;App=EntityFramework;'";
+        private readonly string conn = "metadata=res://*/sampleDbEntities.csdl|res://*/sampleDbEntities.ssdl|res://*/sampleDbEntities.msl;provider=System.Data.SqlClient;provider connection string=';data source=" +
+                                       "tcp:aui20192020.database.windows.net,1433;Initial Catalog=sambleDb;Persist Security Info=False;User ID=sql-admin;Password=FitAdm1234;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;App=EntityFramework;'";
 
         public List<Customer> GetAllCustomers()
         {
--- DbAccess/sampleDbEntities.Designer.cs
+++ DbAccess/sampleDbEntities.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Michał\source\repos\Aui2019\DbAccess\sampleDbEntities.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Michał\Documents\_git\Aui_2019_repo\DbAccess\sampleDbEntities.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
